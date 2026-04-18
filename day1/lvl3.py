960df1e49f7076038e0cbcf494afb5a1c46af30f
MrAlikhanov <alialixanov2007@gmail.com>
2026-04-18 23:34:01 +0400

    Create lvl3.py

---
+++ day1/lvl3.py
@@ -0,0 +1,11 @@
+print(25)              # Integer
+print(9.81)            # Float
+print(3 + 5j)          # Complex
+
+print("Salam Dünya")   # String
+print(True)            # Boolean (Doğru)
+print(False)           # Boolean (Yalan)
+print([1, 2, 3, 4])    # List (Siyahı)
+print((5, 10, 15))     # Tuple (Dəyişməyən siyahı)
+print({1, 2, 3, 3})    # Set (Unikal çoxluq)
+print({'dil': 'Python', 'gün': 30}) # Dictionary (Lüğət)
