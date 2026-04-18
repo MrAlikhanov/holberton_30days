1eceb17c5fae1d9a543b880c1ba125562e0db10c
MrAlikhanov <alialixanov2007@gmail.com>
2026-04-18 23:35:34 +0400

    Update lvl2.py

--- day1/lvl2.py
+++ day1/lvl2.py
@@ -7,8 +7,8 @@ print(3 / 4)    # division(/)
 print(3 ** 4)   # exponential(**)
 print(3 // 4)   # floor division operator(//)
 
-print("Sənin Adın")
-print("Sənin Soyadın")
+print("Ad")
+print("Soyad")
 print("Azərbaycan")
 print("I am enjoying 30 days of python")
 
@@ -17,13 +17,13 @@ print(type(9.8))                                 # Float
 print(type(3.14))                                # Float
 print(type(4 - 4j))                              # Complex
 print(type(['Asabeneh', 'Python', 'Finland']))  # List
-print(type("Sənin Adın"))                        # String
-print(type("Sənin Soyadın"))                     # String
+print(type("Ad"))                        # String
+print(type("Soyad"))                     # String
 print(type("Azərbaycan"))                        # String
 
 print(3 + 4)
 print(3 - 4)
 print(3 * 4)
 print(3 / 4)
-print("Sənin Adın")
+print("Ad")
 print(type(10))
