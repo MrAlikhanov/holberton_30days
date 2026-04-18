a32a6d88a837ee54f46fb00525fa1830552cd1cb
MrAlikhanov <alialixanov2007@gmail.com>
2026-04-18 23:32:54 +0400

    Create lvl1.py

---
+++ day1/lvl1.py
@@ -0,0 +1,22 @@
+
+print(3 + 4)    # addition(+)
+print(3 - 4)    # subtraction(-)
+print(3 * 4)    # multiplication(*)
+print(3 % 4)    # modulus(%)
+print(3 / 4)    # division(/)
+print(3 ** 4)   # exponential(**)
+print(3 // 4)   # floor division operator(//)
+
+print("Sənin Adın")
+print("Sənin Soyadın")
+print("Azərbaycan")
+print("I am enjoying 30 days of python")
+
+print(type(10))                                  # Integer
+print(type(9.8))                                 # Float
+print(type(3.14))                                # Float
+print(type(4 - 4j))                              # Complex
+print(type(['Asabeneh', 'Python', 'Finland']))  # List
+print(type("Sənin Adın"))                        # String
+print(type("Sənin Soyadın"))                     # String
+print(type("Azərbaycan"))                        # String
